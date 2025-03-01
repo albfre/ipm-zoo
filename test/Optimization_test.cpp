c58a0bcab9c02df610be66ca0ee8c44acaa05cd3
Albin Fredriksson <albin.fredriksson@gmail.com>
2025-03-01 21:09:39 +0100

    Add settings to Lagrangian, add Optimization tests

---
+++ test/Optimization_test.cpp
@@ -0,0 +1,76 @@
+
+#include "Optimization.h"
+
+#include <gtest/gtest.h>
+
+using namespace Optimization;
+
+class OptimizationTest : public ::testing::Test {
+ protected:
+  void SetUp() override {}
+};
+
+TEST_F(OptimizationTest, GetLagrangian) {
+  auto settings = Settings();
+  auto names = VariableNames();
+  auto [lagrangian, variables] = getLagrangian(names, settings);
+  const auto str = lagrangian.toString();
+  EXPECT_NE(str.find(names.s_A), std::string::npos);
+  EXPECT_NE(str.find(names.s_Al), std::string::npos);
+  EXPECT_NE(str.find(names.s_Au), std::string::npos);
+  EXPECT_NE(str.find(names.s_xl), std::string::npos);
+  EXPECT_NE(str.find(names.s_xu), std::string::npos);
+}
+
+TEST_F(OptimizationTest, GetLagrangianLower) {
+  auto settings = Settings();
+  settings.inequalities = Bounds::Lower;
+  settings.variableBounds = Bounds::Lower;
+  auto names = VariableNames();
+  names.s_Au = "t123";
+  auto [lagrangian, variables] = getLagrangian(names, settings);
+  const auto str = lagrangian.toString();
+  EXPECT_NE(str.find(names.s_A), std::string::npos);
+  EXPECT_NE(str.find(names.s_Al), std::string::npos);
+  EXPECT_EQ(str.find(names.s_Au), std::string::npos);
+  EXPECT_NE(str.find(names.s_xl), std::string::npos);
+  EXPECT_EQ(str.find(names.s_xu), std::string::npos);
+}
+
+TEST_F(OptimizationTest, GetLagrangianUpper) {
+  auto settings = Settings();
+  settings.inequalities = Bounds::Upper;
+  settings.variableBounds = Bounds::Upper;
+  auto names = VariableNames();
+  names.s_Al = "g123";
+  auto [lagrangian, variables] = getLagrangian(names, settings);
+  const auto str = lagrangian.toString();
+  EXPECT_NE(str.find(names.s_A), std::string::npos);
+  EXPECT_EQ(str.find(names.s_Al), std::string::npos);
+  EXPECT_NE(str.find(names.s_Au), std::string::npos);
+  EXPECT_EQ(str.find(names.s_xl), std::string::npos);
+  EXPECT_NE(str.find(names.s_xu), std::string::npos);
+}
+
+TEST_F(OptimizationTest, GetLagrangianNoBounds) {
+  auto settings = Settings();
+  settings.inequalities = Bounds::None;
+  settings.variableBounds = Bounds::None;
+  auto names = VariableNames();
+  names.s_Al = "g123";
+  names.s_Au = "t123";
+  names.s_xl = "y123";
+  names.s_xu = "z123";
+  auto [lagrangian, variables] = getLagrangian(names, settings);
+  const auto str = lagrangian.toString();
+  EXPECT_EQ(str.find(names.s_A), std::string::npos);
+  EXPECT_EQ(str.find(names.s_Al), std::string::npos);
+  EXPECT_EQ(str.find(names.s_Au), std::string::npos);
+  EXPECT_EQ(str.find(names.s_xl), std::string::npos);
+  EXPECT_EQ(str.find(names.s_xu), std::string::npos);
+}
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
