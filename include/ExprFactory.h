f64267d9a32b71de51a33e9c73bb4857b719a453
Albin Fredriksson <albin.fredriksson@gmail.com>
2025-03-24 00:21:05 +0100

    Add ExprFactory header

---
+++ include/ExprFactory.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "Expression.h"
+
+namespace Expression::ExprFactory {
+[[nodiscard]] Expr number(const double value);
+[[nodiscard]] Expr namedScalar(std::string_view name);
+[[nodiscard]] Expr namedVector(std::string_view name);
+[[nodiscard]] Expr variable(std::string_view name);
+[[nodiscard]] Expr matrix(std::string_view name);
+[[nodiscard]] Expr symmetricMatrix(std::string_view name);
+[[nodiscard]] Expr diagonalMatrix(Expr expr);
+[[nodiscard]] Expr transpose(Expr expr);
+[[nodiscard]] Expr negate(Expr expr);
+[[nodiscard]] Expr invert(Expr expr);
+[[nodiscard]] Expr log(Expr expr);
+[[nodiscard]] Expr sum(std::vector<Expr> terms);
+[[nodiscard]] Expr product(std::vector<Expr> terms);
+}  // namespace Expression::ExprFactory
